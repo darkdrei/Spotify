7839c1323247a363fd8fc8d3fc3b3917fe7c1f69
pajaro <eugenio.pajaro.ext@nokia.com>
2018-06-24 22:16:13 -0500

    creacion de configuracion central

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 'use strict'
 
 var mongoose = require('mongoose');
+var app = require('./app');
+
+var port =process.env.PORT || 3977;
 
 mongoose.connect('mongodb://localhost:27017/curso_mean2', (error, res)=>{
    if(error){
         throw error;
    } else{
        console.log('La base de datos esta corriendo correctamente');
+       app.listen(port, function(){
+          console.log("Servidor del api de musica escuchando http://localhost:"+port);
+
+       });
    }
 });
