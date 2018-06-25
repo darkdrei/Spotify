166b0a167bc454b4da6c148c1d17693f032f750f
pajaro <eugenio.pajaro.ext@nokia.com>
2018-06-24 23:28:02 -0500

    configuracion de usuario en la clase principal

--- app.js
+++ app.js
@@ -4,12 +4,16 @@ var express = require('express');
 var bodyParser = require('body-parser');
 
 var app = express();
+
+//Cargado de rutas
+var user_routes = require('./routes/user');
+
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 
 // COnfigurar cabeceras http
 
 //rutas base
-
+app.use('/api', user_routes);
 
 module.exports = app;
